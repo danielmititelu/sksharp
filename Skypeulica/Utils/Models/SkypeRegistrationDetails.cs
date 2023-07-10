c4eedc07d647412f17812b709d18b633ab42d4d2
Dumitru-Daniel Mititelu <mttl.daniel@gmail.com>
2023-07-10 21:27:17 +0300

    Delete old code

--- Skypeulica/Utils/Models/SkypeRegistrationDetails.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Skypeulica.Utils.Models
-{
-    public class SkypeRegistrationDetails
-    {
-        public string RawLocation { get; set; }
-        public string Location { get; set; }
-        public string RegistrationToken { get; set; }
-    }
-}
